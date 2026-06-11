2e22a313cdcaaa9b91e82cae4f6148bc5f567180
Betzaida Belén <1725111012@utectulancingo.edu.mx>
2026-06-11 16:16:03 +0000

    UPDATE calculadora

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -1,9 +1,11 @@
 import web
+import math
 
 urls = (
     '/', 'Index',
         '/calculadora', 'Calculadora'
         )
+
 app = web.application(urls, globals())
 render = web.template.render('views')
 
@@ -20,9 +22,39 @@ class Calculadora:
     
     def POST(self):
             formulario = web.input()
+            operacion = formulario.get('operacion')
+            if operacion == "limpiar":
+                return render.calculadora(0.0, 0.0, 0.0)
+            
             numero_1 = float(formulario['numero_1'])
             numero_2 = float(formulario['numero_2'])
-            resultado = numero_1 + numero_2 
+            resultado = 0.0
+            
+            if operacion == "sumar":
+                resultado = numero_1 + numero_2
+            elif operacion == "restar":
+
+                resultado = numero_1 - numero_2
+            elif operacion == "multiplicar":
+                resultado = numero_1 * numero_2
+
+            elif operacion == "dividir":
+                if numero_2 != 0:
+                    resultado = numero_1 / numero_2
+                else:
+                    resultado = "Error: Div entre 0"
+
+            elif operacion == "potencia":
+                resultado = numero_1 ** numero_2
+            
+            elif operacion == "raiz":
+                if numero_1 >= 0:
+                    resultado = math.sqrt(numero_1)
+                else:
+                    resultado = "Error: Raíz negativa"
+            
+            elif operacion == "modulo":
+                resultado = numero_1 % numero_2
 
             print (f"Tipo de dato de numero_1 : {type(numero_1)}")
             print (f"Tipo de dato de numero_2 : {type(numero_2)}")
