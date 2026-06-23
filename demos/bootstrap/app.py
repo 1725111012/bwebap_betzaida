35b2e4bff4ea5c6654969f9eb3cf69cfd8003e0c
Betzaida Belén <1725111012@utectulancingo.edu.mx>
2026-06-23 05:47:28 +0000

    UPDATE bootstrap

--- demos/bootstrap/app.py
+++ demos/bootstrap/app.py
@@ -1,7 +1,11 @@
 import web
 
 urls = (
-    '/', 'Index'
+    '/', 'Index',
+        '/registro', 'Registro',
+        '/lista_contactos', 'ListaContactos'
+
+
 )
 app = web.application(urls, globals())
 render = web.template.render('views')
@@ -9,6 +13,14 @@ render = web.template.render('views')
 class Index:
     def GET(self):
         return render.index()
+    
+class Registro:
+    def GET(self):
+            return render.registro()
+
+class ListaContactos:
+    def GET(self):
+            return render.lista_contactos()
 
 if __name__ == "__main__":
     app.run()
